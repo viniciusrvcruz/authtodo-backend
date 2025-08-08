1d73bdaa68f60c6b4bd9c745b519864cf7113c03
Vinicius Cruz <viniciusrobertopro@gmail.com>
2025-08-08 17:20:34 -0300

    feat: add initial test setup

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,9 +2,19 @@
 
 namespace Tests;
 
+use App\Models\User;
+use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
+use Laravel\Sanctum\Sanctum;
 
 abstract class TestCase extends BaseTestCase
 {
-    //
+    use RefreshDatabase;
+
+    public function actAsUser(?User $user = null): void
+    {
+        $tester = $user ?? User::factory()->create();
+        
+        Sanctum::actingAs($tester);
+    }
 }
